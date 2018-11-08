89e158224c7a7ecdc9beebeca636316aefa2bf99
unknown <nildo.777@gmail.com>
2018-11-08 14:15:27 -0200

    Buscando imagens

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import "package:transparent_image/transparent_image.dart";
+
+final CollectionReference homeRef = Firestore.instance.collection('home');
 
 class HomeTab extends StatelessWidget {
   @override
@@ -27,6 +32,43 @@ class HomeTab extends StatelessWidget {
                 title: const Text("Novidades"),
                 centerTitle: true,
               ),
+            ),
+            FutureBuilder<QuerySnapshot>(
+              future: homeRef.orderBy('pos').getDocuments(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation(Colors.white),
+                      ),
+                    ),
+                  );
+                }
+                else {
+                  return SliverStaggeredGrid.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 1.0,
+                    crossAxisSpacing: 1.0,
+                    staggeredTiles: snapshot.data.documents.map(
+                      (doc) {
+                        return StaggeredTile.count(doc.data["x"], doc.data["y"]);
+                      }
+                    ).toList(),
+                    children: snapshot.data.documents.map(
+                      (doc){
+                        return FadeInImage.memoryNetwork(
+                          placeholder: kTransparentImage,
+                          image: doc.data['image'],
+                          fit: BoxFit.cover,
+                        );
+                      }
+                    ).toList(),
+                  );
+                }
+              },
             )
           ],
         )
